1ebd4b6f63a04d40909c3de77e5946d736c577d6
KaosArgoGames <redrevolver6c@gmail.com>
2023-06-06 17:24:04 -0400

    FUCK

---
+++ Source/ThirdTry/Public/Actor/C_PickupHealth.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/C_PickupBase.h"
+#include "C_PickupHealth.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDTRY_API AC_PickupHealth : public AC_PickupBase
+{
+	GENERATED_BODY()
+	
+};
